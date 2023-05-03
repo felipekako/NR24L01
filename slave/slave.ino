b770e5760a09ff78f047a94d073433e0cc84fac9
felipekako <felipekakosampaio@gmail.com>
2023-05-03 17:16:03 -0300

    criando radio

---
+++ slave/slave.ino
@@ -0,0 +1,14 @@
+#include <SPI.h>
+#include <nRF:24L01.h>
+#include <RF24.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
