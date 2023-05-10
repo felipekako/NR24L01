368044f11a95987f156a583e2da49298195142e7
felipekako <felipekakosampaio@gmail.com>
2023-05-10 17:32:59 -0300

    radio mestre

--- master/master/master.ino
+++ master/master/master.ino
@@ -1,12 +1,27 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
+RF24 radio(9,10);
+const byte endereco[6]"55555";
+
+
+int pinoBotao = 2;
+int estadoBotao = 0;
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(pinoBotao,INPUT_PULLUP);
+  radio.begin();
+  radio.openWritingPipe(endereco);
+  radio.setPALevel(RF24_PA_HIGH);
+  radio.stopListening();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if(digitalRead(pinoBotao) == HIGH){
+  estadoBotao =1;
+  radio.write(&estadoBotao,sizeof()estadoBotao
+    
+  }
 }
--- slave/slave.ino
+++ slave/slave.ino
@@ -1,23 +1,31 @@
-#include <SPI.h>
-#include <nRF:24L01.h>
-#include <RF24.h>
-
-RF24 radio(9,10);
-const byte endereco[6]"55555";
-
-
-int pinoBotao = 2;
-int estadoBotao = 0;
+#include <SPI.h> //INCLUSÃO DE BIBLIOTECA
+#include <nRF24L01.h> //INCLUSÃO DE BIBLIOTECA
+#include <RF24.h> //INCLUSÃO DE BIBLIOTECA
+ 
+RF24 radio(9, 10); //CRIA UMA INSTÂNCIA UTILIZANDO OS PINOS (CE, CSN)     
+const byte endereco[6] = "00001"; //CRIA UM ENDEREÇO PARA ENVIO DOS
+//DADOS (O TRANSMISSOR E O RECEPTOR DEVEM SER CONFIGURADOS COM O MESMO ENDEREÇO)
+ 
+int pinoBotao = 2; //PINO DIGITAL UTILIZADO PELO BOTÃO
+int estadoBotao = 0; //VARIÁVEL PARA ARMAZENAR O ESTADO DO BOTÃO
+ 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(pinoBotao,INPUT_PULLUP);
-  radio.begin();
-  radio.openWritingPipe(endereco);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.stopListening();
+  pinMode(pinoBotao, INPUT_PULLUP); //DEFINE O PINO COMO ENTRADA / "_PULLUP" É PARA ATIVAR O RESISTOR INTERNO
+    //DO ARDUINO PARA GARANTIR QUE NÃO EXISTA FLUTUAÇÃO ENTRE 0 (LOW) E 1 (HIGH)
+  radio.begin(); //INICIALIZA A COMUNICAÇÃO SEM FIO
+  radio.openWritingPipe(endereco); //DEFINE O ENDEREÇO PARA ENVIO DE DADOS AO RECEPTOR
+  radio.setPALevel(RF24_PA_HIGH);  //DEFINE O NÍVEL DO AMPLIFICADOR DE POTÊNCIA
+  radio.stopListening(); //DEFINE O MÓDULO COMO TRANSMISSOR (NÃO RECEBE DADOS)
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+ 
+void loop(){
+  if(digitalRead(pinoBotao) == HIGH){ //SE A LEITURA DO PINO FOR IGUAL A HIGH, FAZ
+    estadoBotao = 1; //VARIÁVEL RECEBE VALOR 1
+    radio.write(&estadoBotao, sizeof(estadoBotao)); //ENVIA AO RECEPTOR OS DADOS
+  }else{ //SENÃO, FAZ
+        if(digitalRead(pinoBotao) == LOW){ //SE A LEITURA DO PINO FOR IGUAL A LOW, FAZ
+        estadoBotao = 0; //VARIÁVEL RECEBE VALOR 0
+        radio.write(&estadoBotao, sizeof(estadoBotao)); //ENVIA AO RECEPTOR OS DADOS
+        }
+  }
 }
